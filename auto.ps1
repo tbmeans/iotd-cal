da0c91de18b87101df3f3f7691409ad04d20bd73
tbmeans <tbmeans2005@gmail.com>
2025-06-17 14:33:00 -0400

    corrected if stmt

--- auto.ps1
+++ auto.ps1
@@ -8,8 +8,12 @@ $copyrt = (($mscard -split 'id="copyright">')[1] -split '<')[0]
 $WALLPAPER_IS_PERMITTED = 'Download this image. Use of this image is restricted to wallpaper only.'
 $IMAGE_IS_NOT_AVAILABLE = 'This image is not available to download as wallpaper.'
 $permis = ((($mscard -split '<ul class="share"><li>')[1] -split 'title="')[1] -split '"')[0]
-if $permis == $WALLPAPER_IS_PERMITTED
+if (  $permis == $WALLPAPER_IS_PERMITTED )
+{
 	$srckey = ($mscard -split 'th\?id=OHR.')[1] -split '_'
 	$source = $srckey[0] + '_' + $srckey[1]
-else if $permis == $IMAGE_IS_NOT_AVAILABLE
+} elseif ( $permis == $IMAGE_IS_NOT_AVAILABLE )
+{
 	$source = ((($iSplit[0] -split '<div class="hp_media_container">')[1] -split 'th\?id=OHR.')[1] -split '&quot;')[0]
+}
+$titled + "`n" + $copyrt + "`n" + $source
